330aba886f480f92c65313401c51b67b446484eb
Nico-dsa <6463@holbertonstudenst.com>
2023-06-12 11:16:32 +0200

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define a class
+"""
+
+
+class Square:
+    """
+    Define a Square
+    """
+    def __init__(self, size):
+    	"""
+    	Init Square with size
+    	"""
+    	self.__size = size
