5a2e5cd0b2a07d000d4b955824adedda974fa641
Nico-dsa <6463@holbertonstudenst.com>
2023-06-12 14:05:08 +0200

    Fix Pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,12 +3,12 @@
 
 
 class Square:
-	"""Define a Square"""
-	def __init__(self, size=0):
-		"""Init Square with size = 0"""
-		if type(size) != int:
-			raise TypeError("size must be an integer")
-		elif size < 0:
-			raise ValueError("size must be >= 0")
-		else:
-			self.__size = size
+    """Define a Square"""
+    def __init__(self, size=0):
+        """Init Square with size = 0"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
