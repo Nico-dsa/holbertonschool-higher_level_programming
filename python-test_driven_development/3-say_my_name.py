49ed55ef3389e5a1b1265a85b518bdb69424e8bc
Nico-dsa <6463@holbertonstudenst.com>
2023-06-15 12:13:07 +0200

    Write a function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
     """Write a function that prints My name is <first name> <last name>"""
-    try:
-        print("My name is {:s}  {:s}".format(first_name, last_name))
-    except:
-        if type(first_name) != str:
-            raise TypeError('first_name must be a string')
-        elif tyoe(last_name) != str:
-            raise TypeError('last_name must be a string')
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+        
+    else:
+        print("My name is {}".format(first_name))
