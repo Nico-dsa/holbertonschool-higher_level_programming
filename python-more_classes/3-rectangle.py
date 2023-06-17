3fce6ee4ea16494e666ca9456a5e2b26559f46b0
Nico-dsa <6463@holbertonstudenst.com>
2023-06-17 12:11:03 +0200

    Fix Pycodestyle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -63,6 +63,6 @@ class Rectangle:
         rect = []
         for i in range(self.__height):
             [rect.append('#') for i in range(self.__width)]
-            if i != self.__height -1:
+            if i != self.__height - 1:
                 rect.append("\n")
         return "".join(rect)
