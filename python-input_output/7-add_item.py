d566e1da0a1b958c7ba5a0f0add2285619505dee
Nico-dsa <6463@holbertonstudenst.com>
2023-06-23 17:31:57 +0200

    Try to fix output task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""import all module"""
+""" import all module """
 import sys
 
 
-save = __import__('5-save_to_json_file.py').save_to_json_file
-load = __import__('6-load_from_json_file.py').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 
 def lists(argument):
-    """add argument"""
+    """ add argument"""
     try:
         value = load("add_item.json")
     except FileNotFoundError:
